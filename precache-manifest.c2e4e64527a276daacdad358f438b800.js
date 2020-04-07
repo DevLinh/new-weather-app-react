863299d940880a64df6f5799a9a34ceba6551680
DevLinh <DevLinh99QB@gmail.com>
2020-04-07 10:49:02 +0700

    Updates

---
+++ precache-manifest.c2e4e64527a276daacdad358f438b800.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25f3f72b8cdaf97f24effd0015a2f08",
+    "url": "/new-weather-app-react/index.html"
+  },
+  {
+    "revision": "6e20ae7e297f39cf44c0",
+    "url": "/new-weather-app-react/static/css/main.edddeda3.chunk.css"
+  },
+  {
+    "revision": "2cf10b33474a70524c49",
+    "url": "/new-weather-app-react/static/js/2.6bf8281d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/new-weather-app-react/static/js/2.6bf8281d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e20ae7e297f39cf44c0",
+    "url": "/new-weather-app-react/static/js/main.51f51023.chunk.js"
+  },
+  {
+    "revision": "289ff5eb0ac5693b3059",
+    "url": "/new-weather-app-react/static/js/runtime-main.e7b03b7a.js"
+  },
+  {
+    "revision": "654a7c8eb657ea478032db30272fdc36",
+    "url": "/new-weather-app-react/static/media/background_vn.654a7c8e.jpg"
+  }
+]);
